62774567ee7cb86eb473ea3d2acef37cd34b30b1
Julian Rijken <mail@julianrijken.com>
2025-11-25 13:26:39 +0100

    voorbeeld push

--- TankGame/character_body_3d.gd
+++ TankGame/character_body_3d.gd
@@ -2,24 +2,21 @@ extends CharacterBody3D
 
 @export var speed = 14
 
-
 var target_velocity = Vector3.ZERO
 
-
 func _physics_process(delta):
-	# We create a local variable to store the input direction.
 	var direction = Vector3.ZERO
 
-	# We check for each move input and update the direction accordingly.
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
-	if Input.is_action_pressed("move_back"):
-		# Notice how we are working with the vector's x and z axes.
-		# In 3D, the XZ plane is the ground plane.
+	if Input.is_action_pressed("move_backwards"):
 		direction.z += 1
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
 		
 	print(direction)
+	
+	velocity = direction
+	move_and_slide()
