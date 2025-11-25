73769daa0401974147a896689e8b7284b9b0ee46
Julian Rijken <mail@julianrijken.com>
2025-11-25 14:18:00 +0100

    Made character double jump

--- TankGame/character_body_3d.gd
+++ TankGame/character_body_3d.gd
@@ -1,8 +1,14 @@
 extends CharacterBody3D
 
 @export var speed = 14
+@export var jump_strenght = 20
+@export var max_extra_jumps : int = 2
 
+var times_jumped = 0
 var target_velocity = Vector3.ZERO
+
+#func _ready() -> void:
+	#Engine.max_fps = 5
 
 func _physics_process(delta):
 	var direction = Vector3.ZERO
@@ -16,7 +22,23 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
 		
-	print(direction)
+	print(times_jumped)
+		
+	if is_on_floor():
+		times_jumped = 0
+		
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor() or times_jumped <= 1:
+			direction.y += jump_strenght
+			times_jumped += 1
+		
 	
-	velocity = direction
+	velocity = direction * speed
+	velocity.y -= 1.81
+	
 	move_and_slide()
+	
+	# Prevent player from falling infenetly by respawning
+	var is_onder_map = position.y <= -10
+	if is_onder_map:
+		position = Vector3(0,0,0)
