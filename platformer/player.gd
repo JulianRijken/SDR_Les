52444001ef287b460ee3e1cf1eba1d3bab241265
Julian Rijken <mail@julianrijken.com>
2026-01-27 12:10:20 +0100

    new stuff

--- platformer/player.gd
+++ platformer/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
 
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -15,8 +16,24 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	# Flip the sprite
+	if direction > 0:
+		sprite.flip_h = false
+	if direction < 0:
+		sprite.flip_h = true	
+	
+	# Play animations
+	if is_on_floor():
+		if direction == 0:
+			sprite.play("idle")
+		else:
+			sprite.play("run")
+	else:
+		sprite.play("jump")
+	
+	# Apply movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
